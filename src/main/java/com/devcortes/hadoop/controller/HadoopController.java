e321597e872c0c1c1a613ee4de2542da793dbf35
CortesV <vitya.semenyuk1995@gmail.com>
2017-06-14 00:52:25 +0300

    Work with hdsf

--- src/main/java/com/devcortes/hadoop/controller/HadoopController.java
+++ src/main/java/com/devcortes/hadoop/controller/HadoopController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devcortes.hadoop.hadoopservice.fieldcount.HadoopDriverFieldCount;
 import com.devcortes.hadoop.hadoopservice.filterbrand.HadoopDriverBrand;
+import com.devcortes.hadoop.hadoopservice.hdfs.HDFSService;
 import com.devcortes.hadoop.hadoopservice.rewritedbotfile.HadoopDriverBrandRewrite;
 
 @RestController
@@ -20,25 +21,54 @@ public class HadoopController {
 	private HadoopDriverBrandRewrite hadoopDriverRewrite;
 
 	@Autowired
-
 	private HadoopDriverBrand hadoopDriver;
+	
+	@Autowired
+	private HDFSService hdfsService;
 
 	@RequestMapping(value = "/fieldcount", method = RequestMethod.GET, produces = "application/json")
 	public Integer fieldcount() throws Exception {
-
 		return hadoopDriverFieldCount.runJob();
 	}
 
 	@RequestMapping(value = "/filter", method = RequestMethod.GET, produces = "application/json")
 	public Integer filter() throws Exception {
-
 		return hadoopDriver.runJob();
 	}
 
 	@RequestMapping(value = "/rewrite", method = RequestMethod.GET, produces = "application/json")
 	public Integer rewrite() throws Exception {
-
 		return hadoopDriverRewrite.runJob();
+	}
+	
+	@RequestMapping(value = "/hdfs/upload", method = RequestMethod.GET, produces = "application/json")
+	public void upload() throws Exception {
+		hdfsService.upload();
+	}
+	
+	@RequestMapping(value = "/hdfs/view", method = RequestMethod.GET, produces = "application/json")
+	public void viewFile() throws Exception {
+		hdfsService.viewFile();
+	}
+	
+	@RequestMapping(value = "/hdfs/list", method = RequestMethod.GET, produces = "application/json")
+	public void listDirectory() throws Exception {
+		hdfsService.listDirectory();
+	}
+	
+	@RequestMapping(value = "/hdfs/create", method = RequestMethod.GET, produces = "application/json")
+	public void createDirectory() throws Exception {
+		hdfsService.createDirectory();
+	}
+	
+	@RequestMapping(value = "/hdfs/download", method = RequestMethod.GET, produces = "application/json")
+	public void downloadFile() throws Exception {
+		hdfsService.downloadFile();
+	}
+	
+	@RequestMapping(value = "/hdfs/delete", method = RequestMethod.GET, produces = "application/json")
+	public void delete() throws Exception {
+		hdfsService.deleteFile();
 	}
 
 }
